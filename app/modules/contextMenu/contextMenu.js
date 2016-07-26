2cf96c86f1bb124249d7def1ac22382622135f22
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-07-26 23:34:10 +0300

    Изменен стиль меню, добавлено управление с клавиатуры

--- app/modules/contextMenu/contextMenu.js
+++ app/modules/contextMenu/contextMenu.js
@@ -2,10 +2,28 @@
 
 angular.module('contextMenu', [])
     .controller('ContextMenuController', function ($scope, $document, $element) {
-        // Элемент меню
+        // Отображается ли меню        
         let isVisible = false;
+        // Все меню/подменю, принадлежащие этому контекстному меню
         let activeMenus = [];
 
+        // Текущее выделенное меню
+        let selectedMenuIndex = 0;        
+        // Выделенная кнопка в текущем меню
+        let selectedButtonIndex = 0;        
+
+        // Возвращает меню на указанном уровне вложенности
+        let getMenuAtDepth = (depth) => {
+            let result = null;
+            angular.forEach(activeMenus, (menu) => {
+                if (menu.depth == depth) {
+                    result = menu;
+                }
+            });
+            return result;
+        };        
+
+        // Нажатие на кнопку меню
         let handleItemClick = (item) => {    
             switch (typeof(item.click)) {
                 case 'function':
@@ -15,11 +33,19 @@ angular.module('contextMenu', [])
                     $scope.$eval(item.click);
                     break;
             }
-
             this.hide();
         };
 
-        let handleItemMouseOver = (event, menu, button, item) => {
+        // Наведение на кнопку меню
+        let handleItemMouseOver = (menu, button, item, fromKeyboard = false) => {
+            // Сброс выделения с клавиатуры
+            if (activeMenus[selectedMenuIndex] && !fromKeyboard) {
+                let selectedMenu = activeMenus[selectedMenuIndex];
+                let selectedButton = selectedMenu.buttons[selectedButtonIndex];
+               	selectedButton.element.removeClass('active');
+                selectedButtonIndex = 0;
+                selectedMenuIndex = 0;
+            }
             // Скрыть все вложенные меню уровнем выше
             let menusToRemove = [];
             angular.forEach(activeMenus, (m) => {
@@ -34,36 +60,50 @@ angular.module('contextMenu', [])
                 let position = angular.element(button).offset();
                 let x = position.left + angular.element(menu.element).width();
                 let y = position.top;
-                this.addMenu(x, y, item.submenu, menu.depth + 1);
+                this.addMenu(x, y, item.submenu, menu.depth + 1, button);
             }
         };
 
         // Добавить меню/подменю
-        this.addMenu = (x, y, items, depth = 0) => {
+        /**
+         * Добаить меню/подменю 
+         * @param {Number} x - Позиция меню по горизонтали
+         * @param {Number} y - Позиция меню по вертикали
+         * @param {Array} items - Элементы меню
+         * @param {Number} [0] depth - Уровень вложенности меню
+         * @param {Element} [null] parentButtonElement - Кнопка родительского меню, открывающая это вложенное меню
+         */
+        this.addMenu = (x, y, items, depth = 0, parentButtonElement = null) => {
             let div = angular.element('<div>');
-            div.addClass('btn-group-vertical context-menu');
+            div.addClass('list-group context-menu');
             div.attr({role: 'group'});
             div.css({
                 position: 'absolute',
                 left: x + 'px',
-                top: y + 'px'
+                top: y + 'px',
+                // Меню должно всегда быть сверху
+                'z-index': 99999
             });
 
             let menu = {
                 element: div, 
-                depth // Уровень вложенности
+                buttons: [],
+                depth, // Уровень вложенности
+                parentButtonElement
             };
 
             // Добавить кнопки
             angular.forEach(items, (item) => {
-                let button = angular.element('<button>');
-                button.addClass('btn btn-default text-left context-menu-item');                
-                button.append(item.text);
-               	if (item.submenu) {
+                let button = angular.element('<a>');
+                button.addClass('list-group-item context-menu-item');  
+                button.append(item.text + '&nbsp;');
+                button.attr({href: '#'});
+
+                if (item.submenu) {
                     let span = angular.element('<span>');
-                    span.addClass('glyphicon glyphicon-triangle-right');
+                    span.addClass('glyphicon glyphicon-triangle-right pull-right');
                     button.append(span);
-                }
+                }                
                 div.append(button);
 
                 // Включена ли кнопка
@@ -78,8 +118,10 @@ angular.module('contextMenu', [])
                 // Обработка мыши
                 if (isEnabled) {                    
                     button.on('click', (event) => handleItemClick(item));
-                    button.on('mouseover', (event) => handleItemMouseOver(event, menu, button, item));
+                    button.on('mouseover', (event) => handleItemMouseOver(menu, button, item));
                 }
+
+                menu.buttons.push({element: button, item});
             });
 
             // Добавить меню
@@ -99,13 +141,18 @@ angular.module('contextMenu', [])
             menu.element.remove();
         };
 
-        // Отобразить контекстное меню
+        /**
+         * Отобразить это контекстное меню
+         * @param {Number} x - позиция меню по горизонтали
+         * @param {Number} y - позиция меню по вертикали
+         * @param {Array} items - элементы меню
+         */
         this.show = (x, y, items) => {
             this.addMenu(x, y, items);
             isVisible = true;
         };
         
-        // Удалить контекстное меню
+        // Скрыть контекстное меню
         this.hide = () => {
             if (!isVisible) {
                 return;
@@ -121,6 +168,7 @@ angular.module('contextMenu', [])
             if (!isVisible) {
                 return;
             }
+            // Если был клик по меню, не скрывать его
             if (angular.element(event.target).hasClass('context-menu') || 
                 angular.element(event.target).hasClass('context-menu-item')) 
             {
@@ -128,9 +176,87 @@ angular.module('contextMenu', [])
             }
             this.hide();
         });
+
+        // Управление с клавиатуры
+        
+        $document.on('keydown', (event) => {
+            if (!isVisible) {
+                return;
+            }
+            let selectedMenu = activeMenus[selectedMenuIndex];
+            let selectedButton = selectedMenu.buttons[selectedButtonIndex];
+            let previousSelectedButtonIndex = selectedButtonIndex;
+            switch (event.keyCode) {
+                // Left
+                case 37:
+                    // Получение родительского меню
+                    let parentMenu = getMenuAtDepth(selectedMenu.depth - 1);
+                    if (parentMenu) {
+                        let buttonIndex = 0;
+                        // Поиск кнопки, которая открывает это вложенное меню
+                        angular.forEach(parentMenu.buttons, (button, index) => {
+                            if (button.element == selectedMenu.parentButtonElement) {
+                                buttonIndex = index;
+                                return;
+                            }
+                        });
+                        // Переход на родительское меню
+                        selectedMenuIndex = activeMenus.indexOf(parentMenu);
+                        selectedMenu = activeMenus[selectedMenuIndex];
+                        previousSelectedButtonIndex = -1;
+                        selectedButtonIndex = buttonIndex;
+                    } 
+                    break;
+                // Right
+                case 39:
+                    if (selectedButton.item.submenu) {
+                        // Получение вложенного меню
+                        let nestedMenu = getMenuAtDepth(selectedMenu.depth + 1);
+                        if (nestedMenu) {
+                            // Переход во вложенное меню
+                            selectedMenuIndex = activeMenus.indexOf(nestedMenu);
+                            selectedMenu = activeMenus[selectedMenuIndex];
+                            previousSelectedButtonIndex = -1;
+                            selectedButtonIndex = 0;
+                        } 
+                    }
+                    break;
+                // Up
+                case 38:
+                    // Перемещение вверх по текущему меню
+                    selectedButtonIndex--;
+                    if (selectedButtonIndex < 0) {
+                        selectedButtonIndex = selectedMenu.buttons.length - 1;
+                    }                    
+                    break;
+                // Down
+                case 40:
+                    // Перемещение вниз по текущему меню
+                    selectedButtonIndex++;
+                    if (selectedButtonIndex >= selectedMenu.buttons.length) {
+                        selectedButtonIndex = 0;
+                    }               
+                    break;
+                // Enter
+                case 13:
+                    // Выбор элемента меню
+                    handleItemClick(selectedButton.item);
+                    break;
+            }
+            if (selectedButtonIndex != previousSelectedButtonIndex) {
+                event.preventDefault();
+                // Убрать класс с предыдущей выделенной кнопки
+                selectedButton.element.removeClass('active');
+                // Новая выделенная кнопка
+                selectedButton = selectedMenu.buttons[selectedButtonIndex];
+                selectedButton.element.addClass('active');
+                // Имитация наведения мыши
+                handleItemMouseOver(selectedMenu, selectedButton.element, selectedButton.item, true);
+            }
+        });
     })
 
-    .directive('contextMenu', function($document, $rootScope) {
+    .directive('contextMenu', function($rootScope) {
         return {
             controller: 'ContextMenuController',
             link: (scope, element, attrs, controller) => {
@@ -138,10 +264,11 @@ angular.module('contextMenu', [])
                     $rootScope.$apply(function() {
                         event.preventDefault();
                         event.stopPropagation();
-
+                        // Скрыть текущее меню, если оно есть
                         if ($rootScope.activeContextMenuController) {
                             $rootScope.activeContextMenuController.hide();
                         }
+                        // Отобразить меню
                         let items = scope.$eval(attrs.contextMenu);
                         controller.show(event.pageX, event.pageY, items);
                         $rootScope.activeContextMenuController = controller;
